86df7cdc40bd9aba841f9ca09c0fabe389999a72
Paul Banks <banks@banksco.de>
2019-02-20 12:38:22 +0000

    Simple tool for getting snapshot stats

---
+++ README.md
@@ -0,0 +1,38 @@
+# Consul Snapshot Inspection Tool
+
+This is a quick and dirty tool for reading in a [Consul](https://www.consul.io) snapshot file from the raft directory and dumping some statistics about which types of data are consuming the space.
+
+It's fairly basic and quick and can certainly be made easier to use - PRs welcome. We may consider merging this into a more official tool or even the Consul binary if it prooves useful.
+
+## Building
+
+If you have a working Go toolchain you should be able to install this with:
+
+```
+$ go get -u github.com/banks/consul-snapshot-tool
+```
+
+If you want to cross compile it for Linux from another OS (e.g. so you can run the tool on a server where the snapshot file is without moving it or installing Go on the server):
+
+ 1. Checkout this repo into your `$GOPATH`. (Go modules may also work with go 1.12+, not tried yet).
+ 2. Compile with `GOOS=linux go build .`. Assuming your server has same CPU architecture as the server - if not checkout another resource on cross-compiling Go, it's not hard!
+ 3. Copy the `consul-snapshot-tool` binary to the linux server and run it there.
+
+ ## Usage
+
+ There is only one way to use this an no options currently. It reads from STDIN so:
+
+ ```sh
+ $ cat /tmp/consul/raft/sna....32/state.bin | consul-snapshot-tool
+           Record Type    Count   Total Size
+---------------------- -------- ------------
+                   KVS     4294      489.7KB
+              Register      104         57KB
+             Tombstone      530       55.4KB
+               Session       26        4.5KB
+                 Index        9         220B
+             Autopilot        1         188B
+ CoordinateBatchUpdate        1         167B
+---------------------- -------- ------------
+                         TOTAL:      607.2KB
+ ```
---
+++ dumpsnap.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/go-msgpack/codec"
+)
+
+// snapshotHeader is the first entry in our snapshot
+type snapshotHeader struct {
+	// LastIndex is the last index that affects the data.
+	// This is used when we do the restore for watchers.
+	LastIndex uint64
+}
+
+type typeStats struct {
+	Name       string
+	Sum, Count int
+}
+
+type statSlice []typeStats
+
+func (s statSlice) Len() int { return len(s) }
+
+// Less sorts by size descending
+func (s statSlice) Less(i, j int) bool { return s[i].Sum > s[j].Sum }
+func (s statSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+var typeNames []string
+
+func init() {
+	typeNames = []string{
+		"Register",
+		"Deregister",
+		"KVS",
+		"Session",
+		"ACL (Deprecated)",
+		"Tombstone",
+		"CoordinateBatchUpdate",
+		"PreparedQuery",
+		"Txn",
+		"Autopilot",
+		"Area",
+		"ACLBootstrap",
+		"Intention",
+		"ConnectCA",
+		"ConnectCAProviderState",
+		"ConnectCAConfig",
+		"Index",
+		"ACLTokenSet",
+		"ACLTokenDelete",
+		"ACLPolicySet",
+		"ACLPolicyDelete",
+	}
+}
+
+type countingReader struct {
+	r    io.Reader
+	read int
+}
+
+func (r *countingReader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	if err == nil {
+		r.read += n
+	}
+	return n, err
+}
+
+func main() {
+
+	// msgpackHandle is a shared handle for encoding/decoding msgpack payloads
+	var msgpackHandle = &codec.MsgpackHandle{
+		RawToString: true,
+	}
+
+	stats := make(map[int]typeStats)
+
+	cr := &countingReader{r: os.Stdin}
+
+	dec := codec.NewDecoder(cr, msgpackHandle)
+
+	// Read in the header
+	var header snapshotHeader
+	if err := dec.Decode(&header); err != nil {
+		panic(err)
+	}
+
+	// Populate the new state
+	msgType := make([]byte, 1)
+	offset := 0
+	for {
+		// Read the message type
+		_, err := cr.Read(msgType)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		// Decode
+		s := stats[int(msgType[0])]
+		if s.Name == "" {
+			s.Name = typeNames[int(msgType[0])]
+		}
+
+		var val interface{}
+
+		err = dec.Decode(&val)
+		if err != nil {
+			panic(err)
+		}
+
+		// See how big it was
+		size := cr.read - offset
+
+		s.Sum += size
+		s.Count++
+		offset += size
+
+		stats[int(msgType[0])] = s
+	}
+
+	// Output stats in size-order
+	ss := make(statSlice, 0, len(stats))
+
+	for _, s := range stats {
+		ss = append(ss, s)
+	}
+
+	// Sort the stat slice
+	sort.Sort(ss)
+
+	fmt.Printf("% 22s % 8s % 12s\n", "Record Type", "Count", "Total Size")
+	fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
+	for _, s := range ss {
+		fmt.Printf("% 22s % 8d % 12s\n", s.Name, s.Count, ByteSize(uint64(s.Sum)))
+	}
+	fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
+	fmt.Printf("%s % 8s % 12s\n", strings.Repeat(" ", 22), "TOTAL:", ByteSize(uint64(offset)))
+}
+
+const (
+	BYTE = 1 << (10 * iota)
+	KILOBYTE
+	MEGABYTE
+	GIGABYTE
+	TERABYTE
+)
+
+// ByteSize returns a human-readable byte string of the form 10M, 12.5K, and so forth.  The following units are available:
+//	T: Terabyte
+//	G: Gigabyte
+//	M: Megabyte
+//	K: Kilobyte
+//	B: Byte
+// The unit that results in the smallest number greater than or equal to 1 is always chosen.
+// From https://github.com/cloudfoundry/bytefmt/blob/master/bytes.go
+func ByteSize(bytes uint64) string {
+	unit := ""
+	value := float64(bytes)
+
+	switch {
+	case bytes >= TERABYTE:
+		unit = "TB"
+		value = value / TERABYTE
+	case bytes >= GIGABYTE:
+		unit = "GB"
+		value = value / GIGABYTE
+	case bytes >= MEGABYTE:
+		unit = "MB"
+		value = value / MEGABYTE
+	case bytes >= KILOBYTE:
+		unit = "KB"
+		value = value / KILOBYTE
+	case bytes >= BYTE:
+		unit = "B"
+	case bytes == 0:
+		return "0"
+	}
+
+	result := strconv.FormatFloat(value, 'f', 1, 64)
+	result = strings.TrimSuffix(result, ".0")
+	return result + unit
+}
