66d90bda6cf2f365c8ecb8eaebf953ae8cf1b1c1
Joel Watson <joel@watsonian.net>
2020-11-02 16:02:29 -0600

    Add KV size breakdown.

--- dumpsnap.go
+++ dumpsnap.go
@@ -23,13 +23,22 @@ type typeStats struct {
 	Sum, Count int
 }
 
-type statSlice []typeStats
+type kvStats struct {
+	Prefix     string
+	Sum, Count int
+}
 
-func (s statSlice) Len() int { return len(s) }
+type statSlice []typeStats
+type kstatSlice []kvStats
+
+func (s statSlice) Len() int  { return len(s) }
+func (s kstatSlice) Len() int { return len(s) }
 
 // Less sorts by size descending
-func (s statSlice) Less(i, j int) bool { return s[i].Sum > s[j].Sum }
-func (s statSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s statSlice) Less(i, j int) bool  { return s[i].Sum > s[j].Sum }
+func (s statSlice) Swap(i, j int)       { s[i], s[j] = s[j], s[i] }
+func (s kstatSlice) Less(i, j int) bool { return s[i].Sum > s[j].Sum }
+func (s kstatSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 var typeNames []string
 
@@ -96,6 +105,8 @@ func main() {
 
 	stats := make(map[int]typeStats)
 
+	kstats := make(map[string]kvStats)
+
 	cr := &countingReader{r: os.Stdin}
 
 	dec := codec.NewDecoder(cr, msgpackHandle)
@@ -144,6 +155,33 @@ func main() {
 		s.Count++
 		offset += size
 
+		if s.Name == "KVS" {
+			switch val := val.(type) {
+			case map[interface{}]interface{}:
+				// depth controls how many levels deep we keep separate
+				// kv stats for in the breakdown. this should probably
+				// be a CLI option at some point.
+				for k, v := range val {
+					depth := 2
+					if k == "Key" {
+						split := strings.Split(v.(string), "/")
+						if depth > len(split) {
+							depth = len(split)
+						}
+						keys := split[0:depth]
+						prefix := strings.Join(keys, "/")
+						kvs := kstats[prefix]
+						if kvs.Prefix == "" {
+							kvs.Prefix = prefix
+						}
+						kvs.Sum += size
+						kvs.Count++
+						kstats[prefix] = kvs
+					}
+				}
+			}
+		}
+		// fmt.Printf("%v\n", kstats)
 		stats[int(msgType[0])] = s
 	}
 
@@ -157,13 +195,41 @@ func main() {
 	// Sort the stat slice
 	sort.Sort(ss)
 
-	fmt.Printf("% 22s % 8s % 12s\n", "Record Type", "Count", "Total Size")
-	fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
+	fmt.Printf("%s\n", strings.Repeat("-", 52))
+	fmt.Println("RECORD SUMMARY")
+	fmt.Printf("%s\n", strings.Repeat("-", 52))
+	fmt.Printf("% 30s % 8s % 12s\n", "Record Type", "Count", "Total Size")
+	fmt.Printf("%s %s %s\n", strings.Repeat("-", 30), strings.Repeat("-", 8), strings.Repeat("-", 12))
 	for _, s := range ss {
-		fmt.Printf("% 22s % 8d % 12s\n", s.Name, s.Count, ByteSize(uint64(s.Sum)))
+		fmt.Printf("% 30s % 8d % 12s\n", s.Name, s.Count, ByteSize(uint64(s.Sum)))
 	}
-	fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
-	fmt.Printf("%s % 8s % 12s\n", strings.Repeat(" ", 22), "TOTAL:", ByteSize(uint64(offset)))
+	fmt.Printf("%s %s %s\n", strings.Repeat("-", 30), strings.Repeat("-", 8), strings.Repeat("-", 12))
+	fmt.Printf("%s % 8s % 12s\n", strings.Repeat(" ", 30), "TOTAL:", ByteSize(uint64(offset)))
+
+	if len(kstats) > 0 {
+		fmt.Println()
+
+		// Output key stats in size-order
+		ks := make(kstatSlice, 0, len(kstats))
+
+		for _, s := range kstats {
+			ks = append(ks, s)
+		}
+
+		// Sort the key stat slice
+		sort.Sort(ks)
+
+		fmt.Printf("%s\n", strings.Repeat("-", 44))
+		fmt.Println("KEY SIZE BREAKDOWN")
+		fmt.Printf("%s\n", strings.Repeat("-", 44))
+		fmt.Printf("% 22s % 8s % 12s\n", "Key Prefix", "Count", "Total Size")
+		fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
+		for _, s := range ks {
+			fmt.Printf("% 22s % 8d % 12s\n", s.Prefix, s.Count, ByteSize(uint64(s.Sum)))
+		}
+		fmt.Printf("%s %s %s\n", strings.Repeat("-", 22), strings.Repeat("-", 8), strings.Repeat("-", 12))
+		fmt.Printf("%s % 8s % 12s\n", strings.Repeat(" ", 22), "TOTAL:", ByteSize(uint64(offset)))
+	}
 }
 
 const (
